9cd584f85f73277c220ff45d2aee0ecff6b07315
aarav <21202@wc.school.nz>
2025-03-11 17:47:59 +1300

    Updates: - added a login page - changed student and tutor buttons to signup and login - started login feature - started signup feature

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import sqlite3
 from sqlite3 import Error
 
@@ -9,10 +9,28 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return render_template("index.html")
 
-@app.route('/sel')
+@app.route('/sel', methods=['POST', 'GET'])
 def render_selection_page():  # put application's code here
+    if request.method == 'POST':
+        fname = request.form.get("user_fname").title().strip()
+        lname = request.form.get("user_lname").title().strip()
+        email = request.form.get("email").lower().strip()
+        password = request.form.get("password")
+        password2 = request.form.get("password2")
+
+        if password != password2:
+            return redirect("/sel?error=passwords+dont+match")
+
+
+        if len(password) < 8:
+            return redirect("/sel?error=passwords+is+too+short")
+
     return render_template("sel.html")
 
+
+@app.route('/log', methods=['POST', 'GET'])
+def render_login_page():  # put application's code here
+    return render_template("log.html")
 
 if __name__ == '__main__':
     app.run()
